3695ab3ee2295095eec87b0052eb5ad988fb3286
ibrahimabdullahi5 <ibrahimabdullahibare@DESKTOP-0DEOD38.localdomain>
2024-05-12 03:37:26 -0900

    done array lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+ let cats=["Milo", "Otis", "Garfield"] 
+    catslength = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+    function destructivelyAppendCat(name) {
+        cats.push(name); 
+    }
+    cats.push("Milo", "Otis", "Garfield",);
+    function  destructivelyPrependCat(name) {
+        cats.unshift(name);
+    }
+ 
+    cats.push("Milo", "Otis", "Garfield",);
+    function  destructivelyRemoveLastCat() {
+        cats.pop(name);
+    }
+    cats.push("Milo", "Otis", "Garfield",);
+    function  destructivelyRemoveFirstCat() {
+        cats.shift(name);
+    }
+    cats.push("Milo", "Otis", "Garfield",);
+    function  appendCat(name) {
+        let catscopy=cats.slice(name);
+        catscopy.push(name);
+        return catscopy;
+    }
+    cats.push("Milo", "Otis", "Garfield",);
+    function prependCat(name) {
+        let catscopy=cats.slice(name);
+        catscopy.unshift(name);
+        return catscopy;
+    }
+    cats.push("Milo", "Otis", "Garfield",);
+    function removeLastCat() {
+        let catscopy=cats.slice(name);
+        catscopy.pop(name);
+        return catscopy;
+    }
+    cats.push("Milo", "Otis", "Garfield",);
+    function removeFirstCat() {
+        let catscopy=cats.slice(name);
+        catscopy.shift(name);
+        return catscopy;
+    }
